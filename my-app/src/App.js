bfa5825455ec0d122ca18cd744b5da14598b98fc
Hendry <hendry82@ui.ac.id>
2020-09-03 21:44:58 +0700

    Finish Tugas 9 :zap:
    Update app.js based on quiz 1 - soal 1, using inline CSS in jsx.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Create react app based on Quiz
+// Use the same inline styling, because just need to change inside app.js
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "40%",
+        margin: "0 auto",
+        border: "1px solid #000",
+        borderRadius: "20px",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>Form Pembelian Buah</h1>
+      <div style={{ padding: "20px", paddingTop: "0px" }}>
+        <div style={{ display: "block", marginBottom: "1em" }}>
+          <div
+            style={{
+              display: "inline-block",
+              width: "150px",
+              fontWeight: "bold",
+              fontSize: "16px",
+            }}
+          >
+            Nama Pelanggan
+          </div>
+          <input
+            style={{ display: "inline-block", type: "text", name: "name" }}
+          />
+        </div>
+        <div style={{ display: "block", marginBottom: "1em" }}>
+          <div
+            style={{
+              display: "inline-block",
+              width: "150px",
+              fontWeight: "bold",
+              fontSize: "16px",
+            }}
+          >
+            Daftar Item
+          </div>
+          <div style={{ display: "inline-block" }}>
+            <input type="checkbox" name="semangka" />
+            <label>Semangka</label>
+            <br />
+            <input type="checkbox" name="jeruk" />
+            <label>Jeruk</label>
+            <br />
+            <input type="checkbox" name="nanas" />
+            <label>Nanas</label>
+            <br />
+            <input type="checkbox" name="salak" />
+            <label>Salak</label>
+            <br />
+            <input type="checkbox" name="anggur" />
+            <label>Anggur</label>
+            <br />
+          </div>
+        </div>
+        <button style={{ background: "white", borderRadius: "20px" }}>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            Kirim
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
