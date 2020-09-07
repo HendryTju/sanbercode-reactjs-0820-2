3ff8b9911e01c301187360542f5804506755d435
Hendry <hendry82@ui.ac.id>
2020-09-07 20:00:02 +0700

    Finish Tugas 11 :clock:
    Add folder Tugas-11 with jsx for Clock... It vanishes when reach 0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-import FormulirBuah from "./Tugas-9/formulirbuah.jsx";
-import TabelBuah from "./Tugas-10/tabelbuah.jsx";
+import Clock from "./Tugas-11/clock.jsx";
 
 // Create react app based on Quiz
 // Use the same inline styling, because just need to change inside app.js
@@ -9,8 +8,7 @@ import TabelBuah from "./Tugas-10/tabelbuah.jsx";
 function App() {
   return (
     <div>
-      <FormulirBuah />
-      <TabelBuah />
+      <Clock />
     </div>
   );
 }
